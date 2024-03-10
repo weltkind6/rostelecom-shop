5d96071f5edfa6faa6dcf18e4406e01ff6ef9210
yaroslavzefi <yaroslavzef@gmail.com>
2024-03-10 11:39:45 +0300

    fix: Rostelecom !important font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Layout from '@/components/layouts/Layout'
-import './globalStyles/globals.css'
 import './globalStyles/header.css'
 import './globalStyles/normalize.css'
+import './globalStyles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body>
         <Layout>{children}</Layout>
       </body>
     </html>
