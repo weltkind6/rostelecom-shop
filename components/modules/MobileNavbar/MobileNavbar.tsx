a461e5ac5c2614e17d8b0e02e5b9aeacbf2d5f5d
yaroslavzefi <yaroslavzef@gmail.com>
2024-03-14 11:21:14 +0300

    fix: catalog mobile menu is't open

--- components/modules/MobileNavbar/MobileNavbar.tsx
+++ components/modules/MobileNavbar/MobileNavbar.tsx
@@ -7,6 +7,7 @@ import {
   openMenu,
 } from '@/context/modals'
 import { useLang } from '@/hooks/useLang'
+import CatalogMenu from '../Header/CatalogMenu'
 import { addOverFlowHiddenToBody } from '@/lib/utils/common'
 
 const MobileNavbar = () => {
@@ -19,32 +20,38 @@ const MobileNavbar = () => {
   }
 
   const handleOpenCatalogMenu = () => {
-    addOverFlowHiddenToBody()
+    addOverFlowHiddenToBody('0')
     openCatalogMenu()
     closeMenu()
   }
 
   return (
-    <div className='mobile-navbar'>
-      <Link href='/' className='mobile-navbar__btn'>
-        {translations[lang].breadcrumbs.main}
-      </Link>
-      <button
-        className='btn-reset mobile-navbar__btn'
-        onClick={handleOpenCatalogMenu}
-      >
-        {translations[lang].breadcrumbs.catalog}
-      </button>
-      <Link className='btn-reset mobile-navbar__btn' href='/favorites'>
-        {translations[lang].breadcrumbs.favorites}
-      </Link>
-      <Link className='btn-reset mobile-navbar__btn' href='/cart'>
-        {translations[lang].breadcrumbs.cart}
-      </Link>
-      <button className='btn-reset mobile-navbar__btn' onClick={handleOpenMenu}>
-        {translations[lang].common.more}
-      </button>
-    </div>
+    <>
+      <CatalogMenu />
+      <div className='mobile-navbar'>
+        <Link href='/' className='mobile-navbar__btn'>
+          {translations[lang].breadcrumbs.main}
+        </Link>
+        <button
+          className='btn-reset mobile-navbar__btn'
+          onClick={handleOpenCatalogMenu}
+        >
+          {translations[lang].breadcrumbs.catalog}
+        </button>
+        <Link className='btn-reset mobile-navbar__btn' href='/favorites'>
+          {translations[lang].breadcrumbs.favorites}
+        </Link>
+        <Link className='btn-reset mobile-navbar__btn' href='/cart'>
+          {translations[lang].breadcrumbs.cart}
+        </Link>
+        <button
+          className='btn-reset mobile-navbar__btn'
+          onClick={handleOpenMenu}
+        >
+          {translations[lang].common.more}
+        </button>
+      </div>
+    </>
   )
 }
 
